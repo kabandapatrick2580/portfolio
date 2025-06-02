79a89dbf4e25e01905ce270bcec71a2f96f9fa55
kabanda.patrick2580 <sendsome2019@gmail.com>
2025-06-02 22:37:20 +0200

    implemented the footer content

---
+++ portfolio/components/Footer.tsx
@@ -0,0 +1,27 @@
+import Link from"next/link";
+
+
+export default function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white p-4 mt-auto">
+            <div className="container mx-auto text-center">
+                <p>&copy; {new Date().getFullYear()} Your Name. All rights reserved.</p>
+                <div className="mt-2 space-x-4">
+                    <Link href="/" className="hover:text-gray-300">
+                        Home
+                    </Link>
+                    <Link href="/projects" className="hover:text-gray-300">
+                        Projects
+                    </Link>
+                    <Link href="https://github.com/your-username" target="_blank" className="hover:text-gray-300">
+                        GitHub
+                    </Link>
+                    <Link href="/contact" className="hover:text-gray-300">
+                        Contact Us
+                    </Link>
+                </div>
+            </div>
+        </footer>
+    );
+}
+    
